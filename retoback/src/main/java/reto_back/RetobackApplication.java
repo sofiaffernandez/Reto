b131f42f6c60520d95b63502f0e28f47ad2e64a8
sofiaffernandez <sofiaffeijoo98@gmail.com>
2026-03-14 14:19:33 +0100

    creacion del repo del backend

---
+++ retoback/src/main/java/reto_back/RetobackApplication.java
@@ -0,0 +1,13 @@
+package reto_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetobackApplication.class, args);
+	}
+
+}
